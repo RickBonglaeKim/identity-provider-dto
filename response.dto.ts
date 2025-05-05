4077c4dfc85798296a8b85095511b8a90b083c16
Iscream-Rick <rick.kim@i-screamarts.com>
2025-05-05 21:26:49 +0900

    Added the common response

---
+++ response.dto.ts
@@ -0,0 +1,9 @@
+export default class CommonResponse<T> {
+  public readonly language: string;
+  public readonly timeStamp: number;
+  constructor(language: string) {
+    this.timeStamp = Date.now();
+    this.language = language;
+  }
+  resultData: T | null;
+}

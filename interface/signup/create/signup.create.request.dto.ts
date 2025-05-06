3d132835429929f603a178944182c7c059d31218
Iscream-Rick <rick.kim@i-screamarts.com>
2025-05-06 15:59:07 +0900

    Added a DTO for the signup

---
+++ interface/signup/create/signup.create.request.dto.ts
@@ -0,0 +1,17 @@
+import { MemberDetailCreateRequestSchema } from 'dto/interface/member.detail/create/member.detail.create.request.dto';
+import { MemberPhoneCreateRequestSchema } from 'dto/interface/member.phone/create/member.phone.create.request.dto';
+import { MemberCreateRequestSchema } from 'dto/interface/member/create/member.create.request.dto';
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+
+const SignupCreateRequestSchema = z.object({
+  member: MemberCreateRequestSchema,
+  memberDetail: MemberDetailCreateRequestSchema,
+  memberPhone: MemberPhoneCreateRequestSchema,
+});
+
+export type SignupCreateRequestType = z.infer<typeof SignupCreateRequestSchema>;
+
+export class SignupCreateRequest extends createZodDto(
+  SignupCreateRequestSchema,
+) {}

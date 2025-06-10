123d5e9738084ecacdc3931fadcd14f6b20d05a3
Iscream-Rick <rick.kim@i-screamarts.com>
2025-06-10 18:15:57 +0900

    Updated DTO to get the passport by the header

--- interface/sign.up/request/phone/sign.up.phone.request.create.dto.ts
+++ interface/sign.up/request/phone/sign.up.phone.request.create.dto.ts
@@ -5,7 +5,6 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 const SignUpWithPhoneRequestCreateSchema = z.object({
-  passport: z.string().length(64).optional(), // if the passport exist, this IPD system will return token to issue authorization code.
   member: MemberRequestCreateSchema,
   memberDetail: MemberDetailRequestCreateSchema,
   memberPhone: MemberPhoneRequestCreateSchema,

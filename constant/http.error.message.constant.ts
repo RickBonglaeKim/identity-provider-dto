5f4f2b44ee1f4076f13791bd8497e789409452e8
Iscream-Rick <rick.kim@i-screamarts.com>
2025-06-13 14:47:45 +0900

    Fixed bugs and added docs

--- constant/http.error.message.constant.ts
+++ constant/http.error.message.constant.ts
@@ -5,8 +5,12 @@ const HTTP_ERROR_MESSAGE = {
   COOKIE_NOT_FOUND: 'The cookie was not found',
   PASSWORD_TOKEN_NOT_FOUND: 'The password token to reset was not found',
   VERIFICATION_CODE_NOT_FOUND: 'The verification code was not found',
+
   MEMBER_KEY_NOT_CREATED: 'The member key was not created',
+
   PASSWORD_TOKEN_WAS_NOT_GENERATED: 'The password token was not generated',
+  PASSWORD_TOKEN_WAS_NOT_REMOVED: 'The password token was not removed',
+
   VERIFICATION_CODE_NOT_GENERATED: 'The verification code was not generated',
   PASSWORD_NOT_UPDATED: 'The password was not updated',
 } as const;

32bdcc815aab797a0aba36f7ae97d15447c74e0d
Iscream-Rick <rick.kim@i-screamarts.com>
2025-06-17 10:53:45 +0900

    Added statements

--- enum/withdrawal.reason.enum.ts
+++ enum/withdrawal.reason.enum.ts
@@ -1,9 +1,9 @@
 export const WITHDRAWAL = {
-  CONTENTS: 'WITHDRAWAL.CONTENTS',
-  PRICE: 'WITHDRAWAL.PRICE',
-  SERVICE: 'WITHDRAWAL.SERVICE',
-  DRAWING: 'WITHDRAWAL.DRAWING',
-  ETC: 'WITHDRAWAL.ETC',
+  CONTENTS: 'WITHDRAWAL.CONTENTS', // 컨텐츠 부족
+  PRICE: 'WITHDRAWAL.PRICE', // 유료 서비스 가격이 비쌈
+  SERVICE: 'WITHDRAWAL.SERVICE', // 웹/앱 서비스 이용 불편
+  DRAWING: 'WITHDRAWAL.DRAWING', // 드로잉툴 사용 불편
+  ETC: 'WITHDRAWAL.ETC', // 기타
 } as const;
 
 export type WithdrawalReasons = (typeof WITHDRAWAL)[keyof typeof WITHDRAWAL];
